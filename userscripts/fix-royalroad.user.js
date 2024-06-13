a4e4a756974b6269e106be28f4b8699a7a547973
Thoke B. Jakobsen <thoke@thanku.dk>
2024-06-13 17:28:28 +0200

    first userscripts

---
+++ userscripts/fix-royalroad.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Fix royalroad
+// @namespace    https://thokej.github.io/
+// @version      2024-06-13
+// @description  Fix royalroad, screen reader.
+// @author       ThokeJ
+// @match        https://www.royalroad.com/fiction/*
+// @icon         https://icons.duckduckgo.com/ip2/royalroad.com.ico
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    
+    let q = /(?<ma>(?:-(?:0-){2,})|(?:_{2,})|^(?:<[^>]+>)?(?:\*+)(?:<\/[^>]+>)?$)/gm;
+    document.querySelectorAll('p:not(:has(> [aria-hidden="true"]))').forEach((t) => { if (t.innerHTML.match(q)) t.innerHTML = t.innerHTML.replace(q, "<span aria-hidden=\"true\">$<ma></span>") });
+})();

2597b67973db1f054cc4462e64e444fc59b3e175
Thoke B. Jakobsen <thoke@thanku.dk>
2024-06-14 13:06:16 +0200

    match # also

--- userscripts/fix-royalroad.user.js
+++ userscripts/fix-royalroad.user.js
@@ -12,6 +12,6 @@
 (function () {
     'use strict';
     
-    let q = /(?<ma>(?:-(?:0-){2,})|(?:_{2,})|^(?:<[^>]+>)?(?:\*+)(?:<\/[^>]+>)?$)/gm;
+    let q = /(?<ma>(?:-(?:0-){2,})|(?:_{2,})|^(?:<[^>]+>)?(?:(?:\*|#)+)(?:<\/[^>]+>)?$)/gm;
     document.querySelectorAll('p:not(:has(> [aria-hidden="true"]))').forEach((t) => { if (t.innerHTML.match(q)) t.innerHTML = t.innerHTML.replace(q, "<span aria-hidden=\"true\">$<ma></span>") });
 })();

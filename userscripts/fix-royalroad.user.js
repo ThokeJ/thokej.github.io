c2e21e62e77804689cb3cb601aa6deeb720d5969
Thoke B. Jakobsen <thoke@thanku.dk>
2025-05-02 11:34:44 +0200

    Updated version

--- userscripts/fix-royalroad.user.js
+++ userscripts/fix-royalroad.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fix royalroad
 // @namespace    https://thokej.github.io/
-// @version      2024-08-13.v2
+// @version      2025-05-02
 // @description  Fix royalroad, screen reader.
 // @author       ThokeJ
 // @match        https://www.royalroad.com/fiction/*/chapter/*

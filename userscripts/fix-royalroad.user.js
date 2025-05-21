72d3d1ed386c113681ba02cb3ecb0d463242e590
Thoke B. Jakobsen <thoke@thanku.dk>
2025-05-21 08:19:59 +0200

    Added **em** tag to acceptabel inner-tegs

--- userscripts/fix-royalroad.user.js
+++ userscripts/fix-royalroad.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fix royalroad
 // @namespace    https://thokej.github.io/
-// @version      2025-05-20
+// @version      2025-05-21
 // @description  Fix royalroad, screen reader.
 // @author       ThokeJ
 // @match        https://www.royalroad.com/fiction/*/chapter/*
@@ -13,7 +13,7 @@
     'use strict';
 
     let q = /(?<ma>(?:-(?:0-){2,})|(?:\*(?: \*){2,})|(?:_{2,})|(?:(?:\*|#)+))/gm;
-    document.querySelectorAll(':is(p,div,span):not([aria-hidden="true"]):not(:has(> :not(br,hr)))').forEach((t) => { if (t.innerHTML.match(q)) t.innerHTML = t.innerHTML.replace(q, "<span aria-hidden=\"true\">$<ma></span>") });
+    document.querySelectorAll(':is(p,div,span):not([aria-hidden="true"]):not(:has(> :not(br,hr,em)))').forEach((t) => { if (t.innerHTML.match(q)) t.innerHTML = t.innerHTML.replace(q, "<span aria-hidden=\"true\">$<ma></span>") });
 
     document.body.querySelectorAll(':not(:has(.chapter-content), .chapter-content, .chapter-content *, option), [aria-hidden="true"]').forEach((el) => {el.setAttribute("aria-hidden","true")});
 })();

a1fd61af2b0fafb5fc6dc3614b3fc0e754f0276c
Thoke B. Jakobsen <thoke@thanku.dk>
2025-05-23 10:11:02 +0200

    Small fixes

--- userscripts/common/common.js
+++ userscripts/common/common.js
@@ -2,8 +2,8 @@ class ThoJak {
     static TtsIgnore = class TtsIgnore {
         static CustomPageDecorations() {
             let q = /(?<ma>(?:-(?:0-){2,})|(?:\*(?: \*){2,})|(?:_{2,})|(?:(?:\*|#)+))/gm;
-            document.querySelectorAll(':is(p,div,span):not([aria-hidden="true"]):not(:has(> :not(br,hr,em)))').forEach((t) => {
-                if (t.innerHTML.match(q)) t.innerHTML = t.innerHTML.replace(q, "<span aria-hidden=\"true\">$<ma></span>");
+            document.querySelectorAll(':is(p,div,span):not([aria-hidden="true"]):not(:has(> :not(br,hr,em)))').forEach((el) => {
+                if (el.innerHTML.match(q)) el.innerHTML = el.innerHTML.replace(q, "<span aria-hidden=\"true\">$<ma></span>");
             });
         }
         static NonChapterContent(chapterContent) {
@@ -12,10 +12,9 @@ class ThoJak {
             });
         }
     }
-    static test(path, func){
+    static test(path, func) {
         if (window.location.pathname == path) {
             func();
         }
-
     }
 }

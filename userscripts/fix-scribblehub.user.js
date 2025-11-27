7b3f5c66d136418039ee83d5c528c754ed7e960f
Thoke B. Jakobsen <thoke@thanku.dk>
2025-11-27 17:23:38 +0100

    Added a scribblehub user script

---
+++ userscripts/fix-scribblehub.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Fix scribblehub
+// @namespace    https://thokej.github.io/
+// @version      2025-11-27
+// @description  Fix scribblehub, screen reader.
+// @author       ThokeJ
+// @match        https://www.scribblehub.com/read/*/chapter/*/
+// @icon         https://icons.duckduckgo.com/ip2/scribblehub.com.ico
+// @require      https://thokej.github.io/userscripts/common/common.js
+// @grant        none
+// ==/UserScript==
+/** @import * from "./common/common.js" */
+/* global ThoJak */
+
+(function () {
+    'use strict';
+    ThoJak.TtsIgnore.CustomPageDecorations();
+    ThoJak.TtsIgnore.NonChapterContent('div.chp_raw');
+})();
